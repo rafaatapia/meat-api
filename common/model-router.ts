643ed937fbb892d122985b4faed5f19e7bcaebfe
Rafael Tapia <rafaelsilvatapia@gmail.com>
2019-10-28 22:44:54 -0300

    Commit dos arquivos

---
+++ common/model-router.ts
@@ -0,0 +1,109 @@
+import { Router } from './router';
+import * as mongoose from 'mongoose';
+import { NotFoundError } from 'restify-errors';
+
+export abstract class ModelRouter<D extends mongoose.Document> extends Router {
+
+  basePath: string;
+  pageSize: number = 2;
+
+  constructor(protected model: mongoose.Model<D>) {
+    super();
+    this.basePath = `/${model.collection.name}`
+  }
+
+  protected prepareOne(query: mongoose.DocumentQuery<D, D>): mongoose.DocumentQuery<D, D> {
+    return query;
+  }
+
+  envelope(document: any): any {
+    let resource = Object.assign({ _links: {} }, document.toJSON());
+    resource._links.self = `${this.basePath}/${resource._id}`;
+    return resource;
+  }
+
+  envelopeAll(documents: any[], options: any = {}): any {
+    const resource: any = {
+      _links: {
+        self: `${options.url}`
+      },
+      items: documents
+    }
+    if (options.page && options.count && options.pageSize) {
+      if (options.page > 1) {
+        resource._links.previous = `${this.basePath}?_page=${options.page - 1}`
+      }
+      const remaining = options.count - (options.page * options.pageSize);
+      if (remaining > 0) {
+        resource._links.next = `${this.basePath}?_page=${options.page + 1}`
+      }
+    }
+    return resource;
+  }
+
+  validateId = (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      next(new NotFoundError('Document not found'));
+    } else {
+      next();
+    }
+  }
+
+  findAll = (req, res, next) => {
+    let page = parseInt(req.query._page || 1);
+    page = page > 0 ? page : 1;
+    const skip = (page - 1) * this.pageSize;
+
+    this.model
+      .count({}).exec()
+      .then(count => this.model.find()
+        .skip(skip)
+        .limit(this.pageSize)
+        .then(this.renderAll(res, next, { page, count, pageSize: this.pageSize, url: req.url })))
+      .catch(next);
+  }
+
+  findById = (req, res, next) => {
+    this.prepareOne(this.model.findById(req.params.id))
+      .then(this.render(res, next))
+      .catch(next);
+  }
+
+  save = (req, res, next) => {
+    let document = new this.model(req.body);
+    document.save()
+      .then(this.render(res, next))
+      .catch(next);
+  }
+
+  replace = (req, res, next) => {
+    const options = { runValidators: true, overwrite: true };
+    this.model.update({ _id: req.params.id }, req.body, options).exec().then(result => {
+      if (result.n) {
+        return this.model.findById(req.params.id)
+      } else {
+        throw new NotFoundError('Documento não encontrado');
+      }
+    }).then(this.render(res, next))
+      .catch(next);
+  }
+
+  update = (req, res, next) => {
+    const options = { runValidators: true, new: true };
+    this.model.findByIdAndUpdate(req.params.id, req.body, options)
+      .then(this.render(res, next))
+      .catch(next);
+  }
+
+  delete = (req, res, next) => {
+    this.model.deleteOne({ _id: req.params.id }).exec().then(cmdResult => {
+      if (cmdResult.n) {
+        res.send(204);
+      } else {
+        throw new NotFoundError('Documento não encontrado');
+      }
+      return next();
+    }).catch(next);
+  }
+
+}
---
+++ users/users.router.ts
@@ -0,0 +1,38 @@
+import { ModelRouter } from '../common/model-router'
+import * as restify from 'restify';
+import { NotFoundError } from 'restify-errors';
+import { User } from './users.model';
+
+class UsersRouter extends ModelRouter<User> {
+
+  constructor() {
+    super(User);
+    this.on('beforeRender', document => {
+      document.password = undefined
+    });
+  }
+
+  findByEmail = (req, res, next) => {
+    if (req.query.email) {
+      User.findByEmail(req.query.email)
+        .then(user => user ? [user] : [])
+        .then(this.renderAll(res, next))
+        .catch(next);
+    } else {
+      next();
+    }
+  }
+
+  applyRoutes(application: restify.Server) {
+
+    application.get({ path: `${this.basePath}`, version: '2.0.0' }, [this.findByEmail, this.findAll]);
+    application.get({ path: `${this.basePath}`, version: '1.0.0' }, this.findAll);
+    application.get(`${this.basePath}/:id`, [this.validateId, this.findById]);
+    application.post(`${this.basePath}`, this.save);
+    application.put(`${this.basePath}/:id`, [this.validateId, this.replace]);
+    application.patch(`${this.basePath}/:id`, [this.validateId, this.update]);
+    application.del(`${this.basePath}/:id`, [this.validateId, this.delete]);
+  }
+}
+
+export const usersRouter = new UsersRouter();
